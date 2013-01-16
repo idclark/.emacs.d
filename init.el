fc1a83882f3516c9d186167078eb643c7116343d
Ian Clark <idclark13@gmail.com>
2013-01-15 22:48:07 -0500

    correct implimentation to launch SML repl

--- init.el
+++ init.el
@@ -47,7 +47,7 @@
  "/usr/local/smlnj-110.75/bin" ":"
 
 (getenv "PATH")))
-
+(setq exec-path (cons "/usr/local/smlnj-110.75/bin" exec-path))
 ;;launch skim
 (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
 
