6ebbe015696d37e3717cbbe4c6b154b757ab3a6d
Ian Clark <idclark13@gmail.com>
2014-08-05 15:02:01 -0400

    bring back most of clojure config after cider-0.7

--- clojure-config.el
+++ clojure-config.el
@@ -1,4 +1,4 @@
 (require 'cider)
-;(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'clojure-mode-hook 'paredit-mode)
-;(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
