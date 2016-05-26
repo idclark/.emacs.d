e377462bd2907891f8436bb25b192d3f399a9aa2
idclark <idclark13@gmail.com>
2016-05-25 20:26:39 -0400

    tags for org-mode

--- org-config.el
+++ org-config.el
@@ -2,3 +2,5 @@
 (setq org-log-done 'time)
 (setq org-todo-keywords
   '((sequence "TODO" "IN-PROGRESS" "PENDING" "DONE")))
+(setq org-tag-alist '(("@jobs" . ?j) ("@python" . ?p) ("blogs" . ?b)
+		      ("@ml-stats" . ?ml) ("finance" . ?f)))
