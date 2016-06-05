bba62a2d1e6b1276aefd5a737f0aaa79b58d84f6
idclark <idclark13@gmail.com>
2016-06-05 14:13:49 -0400

    readability

--- org-config.el
+++ org-config.el
@@ -2,7 +2,9 @@
 (setq org-log-done 'time)
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "PENDING" "DONE")))
+
 (setq org-todo-keyword-faces
       '(("IN-PROGRESS" . "orange") ("PENDING" . "yellow")))
+
 (setq org-tag-alist '(("@jobs" . ?j) ("@python" . ?p) ("blogs" . ?b)
 		      ("@ml-stats" . ?ml) ("finance" . ?f)))
