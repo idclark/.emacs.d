2ee920ba9af25ad8b015cd2fb29182a925cb57e5
idclark <idclark13@gmail.com>
2017-01-14 16:37:47 -0500

    remove redundant config files

--- cpp-config.el
+++
@@ -1,3 +0,0 @@
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
--- scala-config.el
+++
@@ -1,8 +0,0 @@
-;; this is entirely reliant on ensime for Apache-Spark work
-
-(use-package ensime
-  :ensure t
-  :pin melpa-stable)
-
-(provide 'scala-config)
-;; scala-config.el ends here
