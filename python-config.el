962d03a2904289576d2662fd80eb069092e48c52
idclark <idclark13@gmail.com>
2017-01-14 11:35:10 -0500

    last commit on master before refactor

--- python-config.el
+++ python-config.el
@@ -1,5 +1,5 @@
 (add-hook 'python-mode-hook 'anaconda-mode)
-(add-hook 'python-mode-hook 'eldoc-mode-hook)
+;(add-hook 'python-mode-hook 'eldoc-mode-hook)
 (add-hook 'python-mode-hook 'ac-anaconda-setup)
 
 (setq jedi:server-args
@@ -28,15 +28,8 @@
           (define-key python-mode-map "\C-c\C-j" 'python-shell-send-statement)))
 ;set ipython as default editor for python.el
 (setq
- python-shell-interpreter "/Users/idclark/anaconda/bin/ipython"
-; python-shell-interpreter-args ""
- ; python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-  ;python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-  python-shell-completion-setup-code
-    "from IPython.core.completerlib import module_completion"
-  python-shell-completion-module-string-code
-    "';'.join(module_completion('''%s'''))\n"
-  python-shell-completion-string-code
-    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args "-i"
+)
 
 (provide 'python-config)
